ff54224ea0b4906660a3057acaf2e4f87f3c0354
Luca Angioloni <lucaangioloni@gmail.com>
2022-01-18 20:17:44 +0100

    finished styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["./index.html", "./js/*.js"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: [
+        "Open Sans, Inter, Roboto, Oxygen, Fira Sans, Helvetica Neue, sans-serif",
+      ],
+      serif: ["serif"],
+    },
   },
   plugins: [],
   darkMode: "class",
